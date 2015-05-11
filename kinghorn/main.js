c11f179089044f14d6140b79561b3f5e088a7ee7
fox <hispanic@hush.ai>
2015-05-11 14:35:04 -0400

    fix dumb js syntax error

--- kinghorn/main.js
+++ kinghorn/main.js
@@ -8,43 +8,43 @@ $(document).ready(function (){
 
   $('#about').click(function(){
     setContentToPage('about');
-  }
+  });
 
   $('#crop-gradient').click(function(){
     setContentToPage('cropAndGradient');
-  }
+  });
 
   $('#sketches').click(function(){
     setContentToPage('sketches');
-  }
+  });
 
   $('#rain').click(function(){
     setContentToPage('rain');
-  }
+  });
 
   $('#triple-frame').click(function(){
     setContentToPage('tripleFrame');
-  }
+  });
 
   $('#pen').click(function(){
     setContentToPage('pen');
-  }
+  });
 
   $('#logos').click(function(){
     setContentToPage('logos');
-  }
+  });
 
   $('#rhino').click(function(){
     setContentToPage('rhino');
-  }
+  });
 
   $('#flash').click(function(){
     setContentToPage('flash');
-  }
+  });
 
   $('#tech').click(function(){
     setContentToPage('tech');
-  }
+  });
 
 });
 
