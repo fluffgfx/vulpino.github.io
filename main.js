1fe7e11a68ece674d4024fd9d8fa2609e64a7686
VULPINE <hispanic@hush.ai>
2015-12-07 13:50:54 -0500

    add on hover name

--- main.js
+++ main.js
@@ -3,22 +3,69 @@ $(document).ready(function(){
   var logostring = "M374.5,385.7 l250,250 h-500 L374.5,385.7 z M374.5,635.7 l353.6,353.6 V635.7 H374.5 z M728.1,687.5 l125,125 l125-123.6 l-125-126.4 L728.1,687.5 z M978.1,688.9 V437.5 l-125,125 L978.1,688.9 z M728.1,436.1 v251.4 l125-125 L728.1,436.1 z M198.7,561.5 l175.8-175.8 l-352.6-1 L198.7,561.5 z M249.5,260.7 l125,125 v-250 l-125-125 V260.7 z"
 	var logo = draw.path(logostring).fill("white").size($(window).height()/(1.5)).center(($(window).width()/2), ($(window).height()/2)).stroke({color: '#fff', opacity: 0.1, width: 1});
 	var image = draw.image("img/background.jpg").size($(window).width()/(1.25), $(window).height()/(1.25)).center(($(window).width()/2), ($(window).height()/2)).clipWith(logo);
+  var scrolled = false;
+  var onLogo = true;
+  var toggling = false;
+
+  var toggleLogo = function() {
+    if(toggling){ return; }
+    toggling = true;
+    if(onLogo){
+      // fade out logo
+      image.animate(50, '>').opacity(0).after(function(){
+        // Hide logo
+        image.hide().unclip();
+        // Toggle to text
+        var kyle = draw.text("Kyle\nFahringer").fill("white");
+        kyle.center(150, 0);
+        image.size($(window).width(), $(window).height());
+        image.center(($(window).width()/3)+150, $(window).height()/2);
+        if(scrolled) {
+          kyle.center(150, 20);
+          kyle.font({
+            family: 'Brandon Grotesque',
+            weight: 900,
+            size: 48
+          });
+        }else{
+          kyle.font({
+            family: 'Brandon Grotesque',
+            weight: 900,
+            size: ($(window).height()/3)
+          });
+        }
+        image.clipWith(kyle).show().animate(150, '>').opacity(1).after(function(){
+          toggling = false;
+        });
+      });
+      onLogo = false;
+    }else{
+      // fade out text
+      image.animate(50, '>').opacity(0).after(function(){
+        // Hide text
+        image.hide().unclip();
+        // Toggle to logo
+        var logo = draw.path(logostring).fill("white")
+        if(scrolled) {
+          logo.size(100).center(175,75);
+        }else{
+          logo.size($(window).height()/(1.5));
+          logo.center($(window).width()/2, $(window).height()/2);
+        }
+        image.clipWith(logo).show().animate(150, '>').opacity(1).after(function(){
+          toggling = false;
+        });
+      });
+      onLogo = true;
+    }
+  }
+
+  $(window).hover(toggleLogo);
 
   $(window).resize(function(e){
     image.hide().unclip();
     logo = draw.path(logostring).fill("white").size($(window).height()/(1.5)).center(($(window).width()/2), ($(window).height()/2)).stroke({color: '#fff', opacity: 0.1, width: 1});
-    image.clipWith(logo).show();
-    image.animate(250, '>', 100).size($(window).width()/(1.25), $(window).height()/(1.25)).center(($(window).width()/2), ($(window).height()/2));
-  });
-
-  var timer;
-  $(window).mousemove(function(e){
-    clearInterval(timer);
-    var pageX = e.pageX
-    var pageY = e.pageY
-    timer = setTimeout(function(){
-      image.animate(250, '>', 100).center(((pageX/16)-($(window).width()/32))+($(window).width()/2), ((pageY/16)-($(window).height()/32))+($(window).height()/2));
-    }, 150);
+    image.clipWith(logo).show().size($(window).width()/(1.25), $(window).height()/(1.25)).center(($(window).width()/2), ($(window).height()/2));
   });
 
   $(window).scroll(function(e){
@@ -27,9 +74,10 @@ $(document).ready(function(){
       logo = draw.path(logostring).fill("white").size($(window).height()/(1.5)).center(($(window).width()/2), ($(window).height()/2)).stroke({color: '#fff', opacity: 0.1, width: 1});
       image.clipWith(logo).show();
       image.size($(window).width()/(1.25), $(window).height()/(1.25)).center(($(window).width()/2), ($(window).height()/2));
-      $("#header").css("opacity", "0")
-    }else if($(window).scrollTop() >= 5 && $(window).scrollTop() <= 105){
-      var percent = -(($(window).scrollTop()-5)/100)+1;
+      $("#header").css("opacity", "0");
+      $("#drawing").css("background-color", "rgba(255,255,255,1)");
+    }else if($(window).scrollTop() >= 5 && $(window).scrollTop() <= 55){
+      var percent = -(($(window).scrollTop()-5)/55)+1;
       var size = ($(window).height()/(1.5))*percent;
       if(size < 100){
         size = 100
@@ -54,14 +102,16 @@ $(document).ready(function(){
       logo = draw.path(logostring).fill("white").size(size).center(centerX, centerY).stroke({color: '#fff', opacity: 0.1, width: 1});
       image.clipWith(logo).show();
       image.size(imageSizeX, imageSizeY).center(centerX, centerY);
-      $(window).off("mousemove");
-      $("#header").css("opacity", (1*(-(percent-1))).toString());
-      $("#drawing").css("background-color", "rgba(255,255,255,"+percent.toString()+")");
     }else{
       image.hide().unclip();
       logo = draw.path(logostring).fill("white").size(100).center(175,75);
       image.clipWith(logo).show();
       image.size(150, 150).center(175,75);
+      $("#header").css("opacity", "1");
+      $("#drawing").css("background-color", "rgba(255,255,255,0)");
+      $(window).off();
+      $("#header").click(toggleLogo);
+      scrolled = true;
     }
   });
 });
